16fef3f319acd774997704ed07eeac748c3c976d
Tristan Isham <tristan@isham.co>
2023-09-03 01:22:59 -0400

    added real logging

--- src/index.tsx
+++ src/index.tsx
@@ -1,33 +1,27 @@
 import { Elysia } from "elysia";
-import { fileLogger } from "@bogeychan/elysia-logger";
-import fs from "fs/promises";
-import process from "process";
 import Home from "./views/home";
 import React from "react";
 import { renderToString } from "react-dom/server";
 import { html } from "@elysiajs/html";
 import { staticPlugin } from "@elysiajs/static";
+import { cookie } from "@elysiajs/cookie";
+
+// Database
+import { logger } from "./lib/logger";
 
 if (import.meta.main) {
-  // Creates log dir
-  const startup = new Date();
-  const logDir =
-    `${process.cwd()}/logs/${startup.getFullYear()}/${startup.getMonth()}/${startup.getDate()}`;
-  await fs.mkdir(logDir, { recursive: true });
 
   const app = new Elysia()
+    .use(cookie())
     .use(staticPlugin())
     .use(html())
-    .use(fileLogger({
-      file:
-        `${logDir}/log-${startup.getFullYear()}-${startup.getMonth()}-${startup.getDate()}.txt`,
-    }))
+    .use(logger())
     .get("/", ({ set }) => {
       set.headers = {
         "Content-Type": "text/html",
       };
 
-      return renderToString(<Home title={"Zig Online!"} />);
+      return renderToString(<Home title={"Helpful"} />);
     }).listen(8080);
 
   console.log(
